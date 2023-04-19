fd6a85155e3b17427e8b58ff24a322d6769a98e6
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-18 18:01:36 -0700

    Retry

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      showCompleted: false,
-    };
-  }
   render() {
     return <div>Todo App</div>;
   }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 export default class Todo extends React.Component {
   render() {
-    return (
-      <div>
-        Todo
-      </div>
-    )
+    return <div>Todo</div>;
   }
 }
