f7f5f324c88cfb117aed97adf1144d6e06465bd2
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-18 17:38:13 -0700

    Work begins

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      showCompleted: false,
+    };
+  }
   render() {
-    return (
-      <div>
-        Todo App
-      </div>
-    )
+    return <div>Todo App</div>;
   }
 }
