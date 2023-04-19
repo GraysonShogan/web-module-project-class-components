638e5843cc4c694826e0f5d386c43ced0115f05d
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-18 18:34:16 -0700

    Working on App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false,
+        },
+        {
+          task: "Bake Cookies",
+          id: 1528817084358,
+          completed: false,
+        },
+      ],
+    };
+  }
   render() {
-    return <div>Todo App</div>;
+    const { todos } = this.state;
+    return (
+      <div>
+        <h1>TODOS</h1>
+
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todos.id}>
+                {todo.task}{" "}
+                {todo.completed ? <span>- completed</span> : <span></span>}
+              </li>
+            );
+          })}
+        </ul>
+
+        <form>
+          <input type="text" />
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
+      </div>
+    );
   }
 }
